c0e04208805fc32ff54d240a01c46f34601feb47
Nikolay Karasev <nomotorolla@gmail.com>
2023-06-12 21:54:36 +0300

    solution to problem #7

--- Seminar1/ConsoleApp4/Program.cs
+++ Seminar1/ConsoleApp4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+	456 -> 6
+	782 -> 2
+	918 -> 8
+*/
+
+Console.Write("Введите целое трехзначное число: ");
+double number = double.Parse(Console.ReadLine());
+
+double number1 = number % 10.0;
+
+Console.Write($"Последняя цифра числа: {number1}");
